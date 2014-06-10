39b7684a03a31e269e6291e1e23f228fb3a24d18
Sahan <keeblersf@gmail.com>
2014-06-10 16:12:55 -0400

    Custom CSS selector added for faster performance than universal selector methods

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,33 @@
-/*!
- * minQuery
- */
+var sweetSelector = {
+
+	select: function (selector) {
+
+		identifySelector(selector)
+
+	}
+
+}
+
+function identifySelector(selector_to_identify){
+
+	if (selector_to_identify.match(/^#/)) {
+		var elementName = selector_to_identify;
+		var modifiedElementName = elementName.split("#").pop();
+		return document.getElementById(modifiedElementName);
+  } else if (selector_to_identify.match(/^\./)) {
+  	var elementName = selector_to_identify;
+		var modifiedElementName = elementName.split(".").pop();
+		return document.getElementByClassName(selector_to_identify)
+	} else {
+		return document.getElementByTagName(selector_to_identify)
+	}
+
+}
+
+var DOM = {}
+
+DOM.hide = {
+
+}
+
+
