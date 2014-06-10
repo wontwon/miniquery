b2b83388b3312438290633d2148ead5abe8b22e9
Tuan Duong <tuanduong15@gmail.com>
2014-06-10 14:56:23 -0400

    Release 0 finished -Module SweetSelector complete

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,13 @@
-/*!
- * minQuery
- */
+// var SweetSelector = {};
+
+// SweetSelector.prototype = {
+//     select: function(element) {
+//         return document.querySelector(element)
+//     }
+// }
+
+var SweetSelector = {
+    select: function(element) {
+        return document.querySelector(element);
+    }
+}
