a067c3395500d5c000aba4b31d7abd2677df6c12
Isabel Rial <isabel.rial@gmail.com>
2014-06-10 15:17:41 -0400

    Release 0 refactor

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,38 +1,8 @@
-var SweetSelector = function() {
-}
+var SweetSelector = {
 
-SweetSelector.prototype.select = function(selector)  {
-	var firstChar = selector.charAt(0);
-	this.selector = selector;
-	switch(firstChar) {
-
-		case '#':
-			return this.id();
-			break;
-
-		case '.':
-			return this.klass();
-			break;
-
-		default:
-			return this.tag();
+	select: function(selector)  {
+		return document.querySelectorAll(selector)[0];	
 	}
-}
-
-SweetSelector.prototype.klass = function() {
-	return document.getElementsByClassName( removeFirstChar(this.selector) );
-}
-
-SweetSelector.prototype.id = function() {
-	return document.getElementById( removeFirstChar(this.selector) );
-}
-
-SweetSelector.prototype.tag = function() {
-	return document.getElementsByTagName( this.selector );
-}
-
-
-var removeFirstChar = function(string) {
-	return string.substr(1, string.length-1);
-}
+};
  
+var DOM = {}; 
