11de411dee073e331c1d62f04255279fa516e455
Isabel Rial <isabel.rial@gmail.com>
2014-06-10 14:41:39 -0400

    Release 0

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,38 @@
-/*!
- * minQuery
- */
+var SweetSelector = function() {
+}
+
+SweetSelector.prototype.select = function(selector)  {
+	var firstChar = selector.charAt(0);
+	this.selector = selector;
+	switch(firstChar) {
+
+		case '#':
+			return this.id();
+			break;
+
+		case '.':
+			return this.klass();
+			break;
+
+		default:
+			return this.tag();
+	}
+}
+
+SweetSelector.prototype.klass = function() {
+	return document.getElementsByClassName( removeFirstChar(this.selector) );
+}
+
+SweetSelector.prototype.id = function() {
+	return document.getElementById( removeFirstChar(this.selector) );
+}
+
+SweetSelector.prototype.tag = function() {
+	return document.getElementsByTagName( this.selector );
+}
+
+
+var removeFirstChar = function(string) {
+	return string.substr(1, string.length-1);
+}
+ 
