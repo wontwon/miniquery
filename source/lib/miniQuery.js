bcde1990ac136aeba589a0f9f5ffbbc82258c1b8
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-10 16:05:49 -0400

    release 1 complete

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,15 @@
-/*!
- * minQuery
- */
+SweetSelector = {
+
+	selectById: function(id) {
+		return document.getElementById(el);
+	},
+
+	selectByClass: function(klass) {
+		return document.getElementsByClassName(klass);		
+	},
+
+	selectByTag: function(tag) {
+		return document.getElementsByTagName(tag);
+	}
+
+}
