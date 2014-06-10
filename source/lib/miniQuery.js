459e4fa71d2c70e0ba2c6d01561f43f6652312eb
Connor Harwood <harwood.connor@gmail.com>
2014-06-10 15:19:51 -0400

    complete release 1

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,5 +1,22 @@
 var SweetSelector = {};
 
 SweetSelector.select = function(cssTag){
-        return document.querySelector(cssTag)
+        return document.querySelector(cssTag);
+}
+
+var DOM = {};
+DOM.displays = {};
+
+DOM.hide = function(cssTag){
+    var curElem = SweetSelector.select(cssTag)
+    DOM.displays[curElem] = curElem.style.display
+    curElem.style.display = "none";
+}
+
+DOM.show = function(cssTag){
+    var curElem = SweetSelector.select(cssTag);
+    var curElemOGDisplay = DOM.displays[curElem]
+    if( curElemOGDisplay ){
+        curElem.style.display = curElemOGDisplay;
+    }
 }
