3a7e1aa915f62c2fe2da4d38365d883c4b075f47
Zach Karst <zach@karst.co>
2014-06-10 18:02:19 -0400

    Dumb mistake

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,21 @@
 /*!
  * minQuery
  */
+
+
+var SweetSelector = {
+
+    select: function( element ) {
+        this.element = element
+        if ( element[0] === "#" ) {
+            this.bind(document.getElementbyId(this.element));
+        }
+        else if ( element[0] === "." ) {
+            this.brind(document.getElementsByClassName(this.element));
+        }
+        else {
+            this.bind(document.getElementsByTagName(this.element));
+        }
+    }
+};
+
