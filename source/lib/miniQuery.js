242b3a61ef717c2faa4ae6ffce7669d127ed2740
Tuan Duong <tuanduong15@gmail.com>
2014-06-10 15:23:48 -0400

    Release 1 complete

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,13 +1,29 @@
-// var SweetSelector = {};
-
-// SweetSelector.prototype = {
-//     select: function(element) {
-//         return document.querySelector(element)
-//     }
-// }
-
 var SweetSelector = {
     select: function(element) {
         return document.querySelector(element);
+    }
+}
+
+
+var DOM = {
+    hide: function(element) {
+        SweetSelector.select(element).style.display = "none";
+    },
+    show: function(element) {
+        SweetSelector.select(element).style.display = "inline";
+    }
+
+    ,
+    addClass: function(element, klass) {
+        SweetSelector.select(element).classList.add(klass)
+    },
+    removeClass: function(element, klass) {
+        SweetSelector.select(element).classList.remove(klass)
+    }
+}
+
+var EventDispatcher = {
+    on: function() {
+
     }
 }
