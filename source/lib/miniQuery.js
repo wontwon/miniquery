07171057a024797175213566feca56e44adf124a
Isabel Rial <isabel.rial@gmail.com>
2014-06-10 15:34:54 -0400

    Release 1

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,8 +1,20 @@
 var SweetSelector = {
-
 	select: function(selector)  {
 		return document.querySelectorAll(selector)[0];	
 	}
 };
  
-var DOM = {}; 
+var DOM = {
+	hide: function(selector) {
+		SweetSelector.select(selector).style.display = "none";
+	},
+	show: function(selector) {
+		SweetSelector.select(selector).style.display = "block";
+	},
+	addClass: function(selector, klass) {
+		SweetSelector.select(selector).classList.add(klass);
+	},
+	removeClass: function(selector, klass) {
+		SweetSelector.select(selector).classList.remove(klass);
+	}
+}; 
