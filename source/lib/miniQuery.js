1d372020927997d01da1bc7cab2653a2c2224bcd
Stephen Roth <stephenroth.sr@gmail.com>
2014-06-10 19:40:39 -0400

    release 3

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -45,3 +45,39 @@ DOM = {
     }
 }
 
+EventDispatcher = {
+    event: {},
+
+    on: function(htmlSelector, eventName, response){
+        this.event = new Event(eventName);
+        var elements = SweetSelector.select(htmlSelector);
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].addEventListener(eventName, response, false);
+        };
+
+    },
+
+    trigger: function(htmlSelector){ // Using only one argument, need more?
+        var elements = SweetSelector.select(htmlSelector);
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].dispatchEvent(this.event);
+        };
+    }
+}
+
+AjaxWrapper = {
+    request: function(options){
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.open(options.type, options.url);
+        httpRequest.send();
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                options.success
+            } else {
+                options.failure
+            }
+        };
+    }
+}
+
+
