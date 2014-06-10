bdcb2968d649dcae2979407982aa1b158c6c6ec2
Connor Harwood <harwood.connor@gmail.com>
2014-06-10 19:33:11 -0400

    add MQ prefix to everything. prepare to reduce namespace

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,41 +1,65 @@
-var SweetSelector = {};
+var MQ = {};
 
-SweetSelector.select = function(cssTag){
-        return document.querySelector(cssTag);
+MQ.SweetSelector = {};
+
+MQ.SweetSelector.select = function(cssTag){
+        return document.querySelectorAll(cssTag);
 }
 
-var DOM = {};
-DOM.displays = {};
+MQ.DOM = {};
+MQ.DOM.displays = {};
 
-DOM.hide = function(cssTag){
-    var curElem = SweetSelector.select(cssTag)
-    DOM.displays[curElem] = curElem.style.display
-    curElem.style.display = "none";
-}
-
-DOM.show = function(cssTag){
-    var curElem = SweetSelector.select(cssTag);
-    var curElemOGDisplay = DOM.displays[curElem]
-    if( curElemOGDisplay ){
-        curElem.style.display = curElemOGDisplay;
+MQ.DOM.hide = function(cssTag){
+    var curElems = MQ.SweetSelector.select(cssTag)
+    for(var i = 0; i < curElems.length; i++){
+        MQ.DOM.displays[curElems[i]] = curElems[i].style.display
+        curElems[i].style.display = "none";
     }
 }
 
-DOM.addClass = function( cssTag, newClass ){
-    SweetSelector.select(cssTag).classList.add(newClass);
+MQ.DOM.show = function(cssTag){
+    var curElems = MQ.SweetSelector.select(cssTag);
+    for(var i = 0; i < curElems.length; i++){
+        var curElemOGDisplay = MQ.DOM.displays[curElems[i]];
+        if( curElemOGDisplay ){
+            curElems[i].style.display = curElemOGDisplay;
+        }else{
+            curElems[i].style.display = "";
+        }
+    }
 }
 
-DOM.removeClass = function( cssTag, dropClass ){
-    SweetSelector.select(cssTag).classList.remove(dropClass);
+MQ.DOM.addClass = function( cssTag, newClass ){
+    MQ.SweetSelector.select(cssTag).classList.add(newClass);
 }
 
-var EventDispatcher = {};
-
-EventDispatcher.on = function(cssTag, triggerName, funktion){
-    SweetSelector.select(cssTag)[triggerName] = funktion;
+MQ.DOM.removeClass = function( cssTag, dropClass ){
+    MQ.SweetSelector.select(cssTag).classList.remove(dropClass);
 }
 
-EventDispatcher.trigger = function(cssTag, triggerName){
-    SweetSelector.select(cssTag)[triggerName]();
+MQ.EventDispatcher = {};
+MQ.EventDispatcher.events = {};
+
+MQ.EventDispatcher.on = function(cssTag, triggerName, funktion){
+    var selectedItems = MQ.SweetSelector.select(cssTag);
+    this.events[triggerName] = new Event(triggerName);
+    for(var i = 0; i < selectedItems.length; i++){
+        selectedItems[i].addEventListener(triggerName, funktion, false);
+    }
 }
 
+MQ.EventDispatcher.trigger = function(cssTag, triggerName){
+    var selectedItems = MQ.SweetSelector.select(cssTag);
+    for(var i = 0; i < selectedItems.length; i++){
+        selectedItems[i].dispatchEvent(this.events[triggerName]);
+    }
+}
+
+MQ.AjaxWrapper = {};
+
+MQ.AjaxWrapper.request = function(options){
+    var oReq = new XMLHttpRequest();
+    oReq.onload = options.success;
+    oReq.open( options.type, options.url, (options.async || true) );
+    oReq.send();
+}
