f3897110d3fb925109386a820268a79d204d88d7
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-10 17:28:26 -0400

    release 1 complete

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,15 +1,46 @@
+rquickExpr = /^(?:#([\w-]+)|(\w+)|\.([\w-]+))$/
+
 SweetSelector = {
-
-	selectById: function(id) {
-		return document.getElementById(el);
-	},
-
-	selectByClass: function(klass) {
-		return document.getElementsByClassName(klass);		
-	},
-
-	selectByTag: function(tag) {
-		return document.getElementsByTagName(tag);
+	select: function(selector) {
+		match = rquickExpr.exec(selector)
+		if (m = match[1]) {
+			return [document.getElementById(m)];	
+		}
+		else if (m = match[2]) {
+			return document.getElementsByTagName(m);			
+		}
+		else if (m = match[3]) {
+			return document.getElementsByClassName(m);
+		}
 	}
+}
 
+DOM = {
+	hide: function(selector) {
+		var elements = SweetSelector.select(selector);
+		for (var i=0; i<elements.length; i++) {
+			elements[i].style.display = "none";
+		}
+	},
+
+	show: function(selector) {
+		var elements = SweetSelector.select(selector);
+		for (var i=0; i<elements.length; i++) {
+			elements[i].style.display = "initial";
+		}
+	},
+
+	addClass: function(selector, klassName) {
+		var elements = SweetSelector.select(selector);
+		for (var i=0; i<elements.length; i++) {
+			elements[i].setAttribute("class", klassName);
+		}
+	},
+
+	removeClass: function(selector, klassName) {
+		var elements = SweetSelector.select(selector);
+		for (var i=0; i<elements.length; i++) {
+			elements[i].removeAttribute("class", klassName);
+		}
+	}
 }
