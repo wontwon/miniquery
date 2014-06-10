d67ae74e2de5ad4a60605ad60dadd658bc99c4d4
Lawrence Manfredi <lrmanfre@me.com>
2014-06-10 18:26:14 -0400

    r1

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -7,3 +7,18 @@ var SweetSelector = {
     return document.querySelectorAll(target)
   }
 }
+
+var DOM = {
+  hide: function(target){
+    (SweetSelector.select(target))[0].setAttribute("style", "display: none;");
+  },
+  show: function(target){
+    (SweetSelector.select(target))[0].setAttribute("style", "display: visible;");
+  },
+  addClass: function(target, class_to_add){
+    (SweetSelector.select(target))[0].classList.add(class_to_add);
+  },
+  removeClass: function(target, class_to_remove) {
+    (SweetSelector.select(target))[0].classList.remove(class_to_remove)
+  }
+}
