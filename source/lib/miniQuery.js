8b2737df9bb81b4fa008312d37acd1d428374b08
Justin Phelps & Ryan Rebo <pair+justinphelps.reboryan@devbootcamp.com>
2014-06-10 20:30:42 -0400

    refactored SweetSelector, got DOM.hide/show working

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -10,34 +10,62 @@ SweetSelector.select = function(type){
 };
 
 SweetSelector.getElement = function(tag){
-  if (tag[0] === "#") {
-    return document.getElementById(tag.replace('#',''));
-} else if (tag[0] === ".") {
-    return document.getElementsByClassName(tag.replace('.',''));
-} else {
-    return document.getElementsByTagName(tag);
-  }
+//   if (tag[0] === "#") {
+//     return document.getElementById(tag.replace('#',''));
+// } else if (tag[0] === ".") {
+//     return document.getElementsByClassName(tag.replace('.',''));
+// } else {
+//     return document.getElementsByTagName(tag);
+//   }
+  return document.querySelectorAll(tag);
 }
 
 DOM = {};
 DOM.hide = function(elements){
-    for (i=0;i < elements.length;i++){
-      DOM._hideElement(elements[i]);
-    } //for
-} //DOM.hide
-
-DOM._hideElement = function(element) {
-  element.style.display = "none";
+  elementList = SweetSelector.select(elements);
+  for (i=0;i < elementList.length;i++){
+    elementList[i].setAttribute("style", "display: none;");
+    // DOM._hideElement(elements[i])
+  }
+}
+DOM.show = function(elements){
+  elementList = SweetSelector.select(elements);
+  for (i=0;i < elementList.length;i++){
+    elementList[i].setAttribute("style", "display: block;");
+  }
 }
 
-DOM.show = function(elements) {
-      for (i=0;i < elements.length;i++){
-        DOM._showElement(elements[i]);
-    } //for
-} //DOM.hide
-
-DOM._showElement = function(element) {
-  element.style.display = "block";
+DOM.addClass = function(elements, addClass){
+  elementList = SweetSelector.select(elements);
+  for (i=0;i < elementList.length;i++){
+    elementList[i].classList.add(addClass);
+  }
 }
+DOM.removeClass = function(elements, removeClass){
+  elementList = SweetSelector.select(elements);
+  for (i=0;i < elementList.length;i++){
+    elementList[i].classList.remove(removeClass);
+  }
+}
+// DOM.hide = function(elements){
+
+//     for (i=0;i < elements.length;i++){
+//       DOM._hideElement(elements[i]);
+//     } //for
+// } //DOM.hide
+
+// DOM._hideElement = function(element) {
+//   element.style.display = "none";
+// }
+
+// DOM.show = function(elements) {
+//       for (i=0;i < elements.length;i++){
+//         DOM._showElement(elements[i]);
+//     } //for
+// } //DOM.hide
+
+// DOM._showElement = function(element) {
+//   element.style.display = "block";
+// }
 
 
