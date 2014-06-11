969b2df3722fb062e85f0b9ac1ac2ec4eb5e0737
Pei Liu & Git Gnarly <pair+girb0t.reidcovington@devbootcamp.com>
2014-06-10 21:47:51 -0400

    release 0

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,20 @@
 /*!
  * minQuery
  */
+var SweetSelector = function() {
+
+}
+
+// SweetSelector.prototype.select = function(){
+
+
+//   }
+// }
+
+SweetSelector.prototype = {
+  select: function(id) {
+    return document.querySelectorAll( id );
+  }
+}
+
+var sweet = new SweetSelector()
