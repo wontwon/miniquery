e582e6e5511c0db447106defb5d881dd7c401d7a
Steven Inouye <steven.inouye91@gmail.com>
2014-06-10 20:58:28 -0400

    have add and remove clas working

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -23,12 +23,18 @@ function findDivsToType(symbol, word){
   }
 }
 
-var Dom = {
+var DOM = {
   hide: function(div){
     SweetSelector.select(div).style.display = 'none';
  }
  , show: function(div){
     SweetSelector.select(div).style.display = null;
+ }
+ , addClass: function(div, classAdd){
+  SweetSelector.select(div).classList.add(classAdd);
+ }
+ , removeClass: function(div, classAdd){
+  SweetSelector.select(div).classList.remove(classAdd);
  }
 }
 
