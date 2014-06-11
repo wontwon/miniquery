29898c6951270ceddfd5d17000b86bb549d1d4f2
Steven Inouye <steven.inouye91@gmail.com>
2014-06-10 21:06:48 -0400

    found querySelector after reading lawrence code

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,25 +1,6 @@
 var SweetSelector= {
   select: function(div){
-  var symbol = findType(div)
-  var word = findWord(div)
-  return findDivsToType(symbol,word);
-  }
-}
-function findType(div){
-   return div[0]
-}
-function findWord(div){
-  return div.slice(1);
-}
-function findDivsToType(symbol, word){
-  if (symbol === '#'){
-    return document.getElementById(word)
-  }
-  else if (symbol === '.'){
-    return document.getElementsByClassName(word)[0];
-  }
-  else {
-    return document.getElementsByTagName(symbol)[0]
+  return document.querySelector(div)
   }
 }
 
