caccfbaa4203189faae8beb60076c2592322e5e4
Pei Liu & Git Gnarly <pair+girb0t.reidcovington@devbootcamp.com>
2014-06-10 22:20:33 -0400

    Release 2 in prog

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -12,9 +12,27 @@ var SweetSelector = function() {
 // }
 
 SweetSelector.prototype = {
-  select: function(id) {
-    return document.querySelectorAll( id );
+  select: function(cssSelector) {
+    return document.querySelectorAll( cssSelector )[0];
   }
 }
 
 var sweet = new SweetSelector()
+
+var DOM = function() {
+
+}
+
+
+DOM.prototype = {
+  displays: {},
+  hide: function(cssSelector) {
+    var selectedElements = SweetSelector.select(cssSelector)
+    for(var i = 0; i < selectedElements.length; i++){
+        DOM.displays[selectedElements[i]] = selectedElements[i].style.display
+        selectedElements[i].style.display = "none";
+        }
+  }
+}
+
+var DOM = new DOM()
